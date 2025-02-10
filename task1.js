d11da812db1c4d876bb4b2a15806eecd1b5b275f
Jeet Limbani <jeetlimbani@Jeets-MacBook-Air.local>
2025-02-10 11:59:46 +0530

    task1

--- task1.js
+++ task1.js
@@ -1,4 +1,10 @@
-console.log("oerng");
-
-
-njcbskdjbjks
+for(let i=1; i<=10; i++){
+switch(true){
+    case i===3:
+        continue; 
+    case i>5:
+        break;
+    default:
+    console.log(i)
+}
+}
